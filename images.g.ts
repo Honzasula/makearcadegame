4588b11565285403a6903954b43e9062eaea6de4
Honzasula <91047795+Honzasula@users.noreply.github.com>
2022-05-10 01:52:38 +0200

    Whole game completed, I actually love it

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "myImage":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`;
             case "image2":
             case "Enemy":return img`
 . . . . . . . . 
@@ -32,6 +13,160 @@ namespace myImages {
 . 5 . . . . 5 . 
 . . . . . . . . 
 . . . . . . . . 
+`;
+            case "image4":
+            case "Ptacek":return img`
+. . 1 . . . . . . . . . . . 
+. . 1 1 1 1 1 . . . 1 1 1 . 
+1 1 1 1 1 1 1 1 1 1 1 f 1 . 
+1 1 1 1 1 1 1 1 1 1 1 1 5 5 
+. . 1 1 1 1 1 1 1 1 1 1 . . 
+. . . . 4 . . . . . . . . . 
+. . . 4 . . . . . . . . . . 
+. . 4 . 4 . . . . . . . . . 
+`;
+            case "image3":
+            case "Zizala":return img`
+3 3 . . . . . . 
+3 3 . . . . . . 
+3 3 b 3 . . . . 
+3 3 b 3 3 3 . . 
+. . . . 3 3 . . 
+. . . . 3 3 . . 
+. . . . 3 3 . . 
+. . . . 3 3 . . 
+. . . . 3 3 3 . 
+. . . . 3 3 3 3 
+. . . . . 3 3 3 
+. . . . . . 3 3 
+. . . . . 3 3 3 
+. 3 f 3 3 b 3 3 
+. 3 3 3 3 b 3 . 
+`;
+            case "image1":
+            case "Doggo OwO":return img`
+. . . . . . e . . 
+. . . . . e e e . 
+. . . . . e e e e 
+e e e e e e e . . 
+. e e e e e . . . 
+. e . . . e . . . 
+. e e . . e e . . 
+`;
+            case "image6":
+            case "Trpaslik":return img`
+. . . 8 8 8 . . 
+. . . f f 8 . . 
+. . . d d . . . 
+9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 
+9 . 9 9 9 9 . 9 
+d . 9 9 9 9 . d 
+d . 9 9 9 9 . d 
+. . 2 . . 2 . . 
+. . 2 . . 2 . . 
+. 2 2 . . 2 2 . 
+`;
+            case "image7":
+            case "Krákoráček":return img`
+f f . . . . . . . . . 
+f f f f . . . . . . . 
+. f 4 4 f . . f 1 . . 
+. f f 4 f f f f f 5 5 
+. . f 4 4 4 4 f . . . 
+. . f f f 4 4 f . . . 
+2 . 2 . . . . . . . . 
+. 2 . . . . . . . . . 
+2 . . . . . . . . . . 
+`;
+            case "image9":
+            case "Fuel":return img`
+2 2 . . . . . . 
+. 2 2 2 2 2 2 . 
+. . 2 2 5 2 2 f 
+. . 2 2 5 2 2 f 
+. . 2 5 5 5 2 f 
+. . 2 5 5 5 2 f 
+. . 2 2 2 2 2 . 
+`;
+            case "image10":
+            case "Munition":return img`
+. . 4 . . 
+. 4 4 4 . 
+4 4 2 4 4 
+4 2 2 2 4 
+4 2 2 2 4 
+4 2 2 2 4 
+4 2 2 2 4 
+4 2 2 2 4 
+4 4 4 4 4 
+`;
+            case "image8":
+            case "Letadýlko":return img`
+1 1 1 . . . . . . . . . . . . . 
+1 1 1 . . . . . . . . . . . . . 
+1 1 1 1 . . . . . . . . . . . . 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+1 1 8 8 1 9 9 1 1 9 9 1 1 9 1 . 
+1 1 8 8 8 8 8 8 8 8 8 8 8 9 1 1 
+1 1 1 8 8 1 1 1 1 1 8 8 8 8 8 1 
+. . . . . 1 1 1 1 1 . . . . . . 
+. . . . 1 1 1 1 1 . . . . . . . 
+. . . . 1 1 1 . . . . . . . . . 
+`;
+            case "image11":
+            case "Meteor1":return img`
+. . . . . d d d d . 
+. . d d d d d d d d 
+. d d d d d d b d d 
+d b b d d d d b d d 
+d b b d d d d d d d 
+d d d d d d d d d d 
+. d d d d d d d d d 
+. d d d d d d b b d 
+. . d d d d b b b d 
+. . . d d d d d d . 
+`;
+            case "image5":
+            case "Meteor2":return img`
+. . . . e e e . . . 
+. . e e e e e e . . 
+. e e e c c e e . . 
+. e e e c c e e e . 
+e e e e e e e e e . 
+e e e e e e e e e e 
+. e e e e e e e e e 
+. e e e e e e e e e 
+. . e c c e e e e e 
+. . e c c e e e e e 
+. . . e e e e e e . 
+`;
+            case "image12":
+            case "Alien1":return img`
+. . . 7 7 7 7 7 7 . . . 
+. 7 7 7 7 7 7 7 7 7 7 . 
+7 7 7 6 6 7 7 6 6 7 7 . 
+7 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 7 7 7 7 7 . 
+. . 7 7 7 7 7 7 7 7 . . 
+. 7 7 7 . . . . 7 7 7 . 
+7 7 7 . . . . . . 7 7 7 
+`;
+            case "image13":
+            case "Alien2":return img`
+. . . 5 5 5 5 . . . . . 
+. . 5 f 5 5 f 5 . . . . 
+. . 5 5 5 5 5 5 . . . . 
+. . 5 5 f f 5 . . . . 5 
+. . . . 5 . . 5 . . 5 5 
+5 5 5 . 5 . 5 5 . 5 5 . 
+. . . 5 5 . 5 . 5 5 . . 
+. . . . 5 5 5 5 5 . . . 
+. 5 5 5 5 . 5 . 5 5 5 5 
+5 5 . . 5 . 5 . . 5 . . 
+5 . . 5 5 . 5 5 . 5 5 . 
+. . 5 5 . . . 5 . . 5 . 
+. . 5 . . . . 5 . . 5 . 
 `;
         }
         return null;
